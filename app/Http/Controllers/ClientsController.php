1588d13239789a80e3b76d472746dfb731486e18
Igor Petrovski <ipetrovski@Igors-MacBook-Air.local>
2022-07-11 22:08:50 +0200

    bookings

--- app/Http/Controllers/ClientsController.php
+++ app/Http/Controllers/ClientsController.php
@@ -21,33 +21,37 @@ class ClientsController extends Controller
         $booking = $bookingHandlingService->make_reservation();
         $booking->update(['client_id' => $client->id, 'car_id' => $car_id]);
         $admin_email = User::first()->email;
-        $this->send_emails($admin_email, $client->email);
+        $this->send_emails($admin_email, $client->email, $booking);
 
         return redirect()->route('home')->with(['success' => 'Your Booking was successfully']);
 
     }
 
-    private function send_emails($admin, $client)
+    private function send_emails($admin, $client, $booking)
     {
-        $for_admin = $this->email_content('admin');
-        $for_client = $this->email_content('client');
+        $for_admin = $this->email_content('admin', $client, $booking);
+        $for_client = $this->email_content('client', $client, $booking);
         Mail::to($admin)->send(new NewBookingEmail($for_admin));
         Mail::to($client)->send(new NewBookingEmail($for_client));
     }
 
-    private function email_content($considered)
+    private function email_content($considered, $client, $booking)
     {
         if ($considered == 'client') {
             return [
                 'title' => 'One Rent a Car | Reservation',
                 'message' => 'Thank you for choosing us for as rental company',
-                'message_two' => 'One of our colleagues will contact you soon so we can confirm the reservation'
+                'message_two' => 'One of our colleagues will contact you soon so we can confirm the reservation',
+                'client' => null,
+                'booking' => null
             ];
         } elseif ($considered == 'admin') {
             return [
                 'title' => 'New Reservation Received',
                 'message' => 'You have received new car booking',
-                'message_two' => "Click here to go the dashboard to see your reservation"
+                'message_two' => "Click here to go the dashboard to see your reservation",
+                'client' => $client,
+                'booking' => $booking
             ];
         }
     }
